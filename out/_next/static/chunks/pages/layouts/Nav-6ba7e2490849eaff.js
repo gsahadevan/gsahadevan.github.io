10ae3d3648954e33f8c361ab4f6c409913013f62
Gauthaman Sahadevan <gsahadevan@outlook.com>
2023-09-22 16:09:40 +0200

    add blog why i use nvim

--- out/_next/static/chunks/pages/layouts/Nav-6ba7e2490849eaff.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[53],{8460:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/layouts/Nav",function(){return u(94)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8460)}),_N_E=n.O()}]);
