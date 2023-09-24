9d96f73cc39f2655636c477d362fb3f3158a69b9
Gauthaman Sahadevan <gsahadevan@outlook.com>
2023-09-24 22:42:22 +0200

    fix: date error

--- pages/utils/format.js
+++ pages/utils/format.js
@@ -1,7 +1,15 @@
 export default function formatDate(date) {
-    return new Intl.DateTimeFormat('en', {
-        month: 'long',
-        day: 'numeric',
-        year: 'numeric',
-    }).format(new Date(date));
+    try {
+        return new Intl.DateTimeFormat('en', {
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric',
+        }).format(new Date(date));
+    } catch (error) {
+        return new Intl.DateTimeFormat('en', {
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric',
+        }).format(new Date());
+    }
 }
