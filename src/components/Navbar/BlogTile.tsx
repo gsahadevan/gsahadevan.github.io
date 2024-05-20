2d140df894725c2f0bca3432937f6c6fda324503
Gauthaman Sahadevan <gsahadevan@outlook.com>
2024-05-20 12:55:57 +0200

    Minor commit

--- src/components/Navbar/BlogTile.tsx
+++ src/components/Navbar/BlogTile.tsx
@@ -3,18 +3,20 @@ import Link from 'next/link';
 
 const BlogTile = ({ post }: { post: any }): JSX.Element => {
     return (
-        <div key={post.slug} className='shadow-lg transition-colors duration-100 ease-in-out rounded-md hover:bg-gray-300 p-4'>
+        <div key={post.slug} className='transition-colors duration-100 ease-in-out hover:bg-gray-100 rounded-lg'>
             <Link href={'/blog/' + post.slug}>
-                <img className='rounded-t-lg' src={post.image} alt='' />
-                <h2 className='text-2xl font-extrabold text-gray-600'>
-                    {post.data.title}
-                </h2>
-                <p className='py-4 leading-7 text-gray-700'>
-                    {post.data.excerpt}
-                </p>
-                <p className='text-sm text-gray-700'>
-                    {formatDate(post.data.date)}
-                </p>
+                <img className='rounded-t-lg' src={post.data.image} alt='' />
+                <div className='p-4 flex flex-col justify-between'>
+                    <h2 className='pb-4 text-2xl font-extrabold text-gray-600'>
+                        {post.data.title}
+                    </h2>
+                    <p className='pb-4 leading-7 text-gray-700'>
+                        {post.data.excerpt}
+                    </p>
+                    <p className='text-sm text-gray-700'>
+                        {formatDate(post.data.date)}
+                    </p>
+                </div>
             </Link>
         </div>
     );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,52 +1,28 @@
 import Link from 'next/link';
-import { MenuIcon } from '@heroicons/react/solid';
 
 export default function Navbar(): JSX.Element {
 
     return (
         <>
-            <nav>
-                <div className='max-w-2xl py-4 px-8 mx-auto lg:max-w-5xl'>
-                    <div className='flex items-center justify-between'>
-                        <Link href='/'>Gauthaman Sahadevan</Link>
-
-                        <ul className='hidden md:flex justify-end space-x-12'>
-                            <li>
-                                <Link href='/blog'>
-                                    <span>Blog</span>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href='/hire-me'>
-                                    <span className='px-6 py-2 text-yellow-100 bg-yellow-600 rounded-full'>
-                                        Hire Me
-                                    </span>
-                                </Link>
-                            </li>
-                        </ul>
-
-                        <div className='md:hidden flex items-center'>
-                            <button>
-                                <MenuIcon className='w-6 h-6' />
-                            </button>
-                        </div>
-                    </div>
+            <nav className='max-w-2xl py-4 px-8 mx-auto lg:max-w-10xl'>
+                <div className='flex items-center justify-between'>
+                    <Link href='/'>Gauthaman Sahadevan</Link>
+                    <ul className='hidden md:flex justify-end space-x-12'>
+                        <li>
+                            <Link href='/blog'>
+                                <span>Blog</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href='/hire-me'>
+                                <span className='px-6 py-2 text-yellow-100 bg-yellow-600 rounded-full'>
+                                    Hire Me
+                                </span>
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
-            <div
-                id='mobile-menu'
-                className='hidden md:hidden flex flex-col absolute w-full px-8 bg-gray-100'>
-                <Link href='/blog'>
-                    <span className='py-2 px-4 text-sm'>
-                        Blog
-                    </span>
-                </Link>
-                <Link href='/hire-me'>
-                    <span className='py-2 px-4 text-sm'>
-                        Hire Me
-                    </span>
-                </Link>
-            </div>
         </>
     );
 }
