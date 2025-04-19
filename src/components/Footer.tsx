fa7d5e45568e15467898e97b1349cfed2eac747a
Gauthaman Sahadevan <gsahadevan@outlook.com>
2025-04-19 12:04:00 +0200

    Fix footer on hover color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,9 +10,9 @@ export const Footer = (): JSX.Element => {
                             <div className="mx-auto max-w-2xl lg:max-w-5xl">
                                 <div className="flex flex-col items-center justify-between gap-6 md:flex-row">
                                     <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-                                        <a className="transition hover:text-teal-500 dark:hover:text-teal-400" href="/">About</a>
-                                        <a className="transition hover:text-teal-500 dark:hover:text-teal-400" href="/blog">Blog</a>
-                                        <a className="transition hover:text-teal-500 dark:hover:text-teal-400" href="/hire-me">Hire Me</a>
+                                        <a className="transition hover:text-rose-500 dark:hover:text-rose-400" href="/">About</a>
+                                        <a className="transition hover:text-rose-500 dark:hover:text-rose-400" href="/blog">Blog</a>
+                                        <a className="transition hover:text-rose-500 dark:hover:text-rose-400" href="/hire-me">Hire Me</a>
                                     </div>
                                     <p className="text-sm text-zinc-400 dark:text-zinc-500">All rights reserved.</p>
                                 </div>
